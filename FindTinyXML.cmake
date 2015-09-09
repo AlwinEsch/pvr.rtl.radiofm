3b3db9f653006845961454648d369495ca815cd4
Alwin Esch <alwin.esch@web.de>
2015-09-10 00:10:43 +0200

    Add missing tinyxml after CMakeLists.txt update and update related file

--- FindTinyXML.cmake
+++ FindTinyXML.cmake
@@ -1,26 +1,27 @@
 # - Find TinyXML
 # Find the native TinyXML includes and library
 #
-#   TINYXML_FOUND       - True if TinyXML found.
-#   TINYXML_INCLUDE_DIR - where to find tinyxml.h, etc.
-#   TINYXML_LIBRARIES   - List of libraries when using TinyXML.
+#   TINYXML_FOUND        - True if TinyXML found.
+#   TINYXML_INCLUDE_DIRS - where to find tinyxml.h, etc.
+#   TINYXML_LIBRARIES    - List of libraries when using TinyXML.
 #
 
-IF( TINYXML_INCLUDE_DIR )
-    # Already in cache, be silent
-    SET( TinyXML_FIND_QUIETLY TRUE )
-ENDIF( TINYXML_INCLUDE_DIR )
+if(PKG_CONFIG_FOUND)
+  pkg_check_modules (TINYXML tinyxml)
+  list(APPEND TINYXML_INCLUDE_DIRS ${TINYXML_INCLUDEDIR})
+endif()
+if(NOT TINYXML_FOUND)
+  find_path( TINYXML_INCLUDE_DIRS "tinyxml.h"
+             PATH_SUFFIXES "tinyxml" )
 
-FIND_PATH( TINYXML_INCLUDE_DIR "tinyxml.h"
-           PATH_SUFFIXES "tinyxml" )
-
-FIND_LIBRARY( TINYXML_LIBRARIES
-              NAMES "tinyxml"
-              PATH_SUFFIXES "tinyxml" )
+  find_library( TINYXML_LIBRARIES
+                NAMES "tinyxml"
+                PATH_SUFFIXES "tinyxml" )
+endif()
 
 # handle the QUIETLY and REQUIRED arguments and set TINYXML_FOUND to TRUE if
 # all listed variables are TRUE
-INCLUDE( "FindPackageHandleStandardArgs" )
-FIND_PACKAGE_HANDLE_STANDARD_ARGS( "TinyXML" DEFAULT_MSG TINYXML_INCLUDE_DIR TINYXML_LIBRARIES )
+include( "FindPackageHandleStandardArgs" )
+find_package_handle_standard_args(TinyXML DEFAULT_MSG TINYXML_INCLUDE_DIRS TINYXML_LIBRARIES )
 
-MARK_AS_ADVANCED( TINYXML_INCLUDE_DIR TINYXML_LIBRARIES )
+mark_as_advanced(TINYXML_INCLUDE_DIRS TINYXML_LIBRARIES)
